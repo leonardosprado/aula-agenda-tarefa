010ed83f8fea3a07d12a401bb8eac5bfe096d6d4
Leonardo <leonardo.prado@soluti.com.br>
2022-07-01 20:35:48 -0300

    primeiro commit

---
+++ src/data/agenda.js
@@ -0,0 +1,37 @@
+export default [
+    {
+        "Titulo": "Aula de Inglês",
+        "Data": "19-06-2022",
+        "HoraInico":"12:00",
+        "HoraFim":"13:00",
+        "Descrição": "Aula de Inglês do básico ao Avançado"
+    },
+    {
+        "Titulo": "Médico",
+        "Data": "20-09-2022",
+        "HoraInico":"12:00",
+        "HoraFim":"13:00",
+        "Descrição": "Clinico Geral"
+    },
+    {
+        "Titulo": "Futebol",
+        "Data": "21-09-2022",
+        "HoraInico":"19:00",
+        "HoraFim":"20:00",
+        "Descrição": "Futebol com a Galera"
+    },
+    {
+        "Titulo": "Aula de Programação",
+        "Data": "23-09-2022",
+        "HoraInico":"19:00",
+        "HoraFim":"20:00",
+        "Descrição": "Ministrar aula de Programação"
+    },
+    {
+        "Titulo": "Aula de Natação",
+        "Data": "24-09-2022",
+        "HoraInico":"18:00",
+        "HoraFim":"20:00",
+        "Descrição": "Aprender a Nadar"
+    }
+]
